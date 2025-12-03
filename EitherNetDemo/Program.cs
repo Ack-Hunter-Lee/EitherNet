76115c69ad94f7294772eadde6351e248571b844
Ack-Hunter-Lee <1348422313@qq.com>
2025-12-03 15:16:17 +0800

    完善demo

--- EitherNetDemo/Program.cs
+++ EitherNetDemo/Program.cs
@@ -1,13 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using EitherNet.Results;
+using System.Net.Http;
+using System.Net.Sockets;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.JavaScript;
 
 Console.WriteLine("Hello, World!");
+
 List<string> errs=new List<string>();
 if (!GetIpInfo().UnWrap(out string ip, err => errs.Add(err)))
     return;
 if (!GetPortInfo().UnWrap(out int port, err => errs.Add(err)))
     return;
+var tcpClient = new TcpClient();
+await tcpClient.ConnectAsync(ip, port);
+
+
 
 
 static IResult<string,string> GetIpInfo() {
