3fff46b0caa1fca653bca055130c0a4a647e9f8b
Ack-Hunter-Lee <1348422313@qq.com>
2025-12-03 15:10:57 +0800

    提交结果模式Demo项目

---
+++ EitherNetDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EitherNet.Results;
+using System.Runtime.CompilerServices;
+
+Console.WriteLine("Hello, World!");
+List<string> errs=new List<string>();
+if (!GetIpInfo().UnWrap(out string ip, err => errs.Add(err)))
+    return;
+if (!GetPortInfo().UnWrap(out int port, err => errs.Add(err)))
+    return;
+
+
+static IResult<string,string> GetIpInfo() {
+    return Success<string, string>.Of("127.0.0.1");
+}
+static IResult<int, string> GetPortInfo()
+{
+    return Success<int, string>.Of(8080);
+}
+
+
