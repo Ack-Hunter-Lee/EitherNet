0f5efdca0b1ef65b5ec6e34c16a5d59aa75645f1
Ack-Hunter-Lee <1348422313@qq.com>
2025-12-11 16:46:46 +0800

    添加对.Net Standard2.0的支持

--- EitherNetDemo/Program.cs
+++ EitherNetDemo/Program.cs
@@ -5,25 +5,50 @@ using System.Net.Sockets;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.JavaScript;
 
-Console.WriteLine("Hello, World!");
+string errMsg = string.Empty;
+try
+{
 
-List<string> errs=new List<string>();
-if (!GetIpInfo().UnWrap(out string ip, err => errs.Add(err)))
-    return;
-if (!GetPortInfo().UnWrap(out int port, err => errs.Add(err)))
-    return;
-var tcpClient = new TcpClient();
-await tcpClient.ConnectAsync(ip, port);
+    if (!GetIpInfo().UnWrap(out string ip, err => errMsg = err))
+        return;
+    if (!GetPortInfo().UnWrap(out int port, err => errMsg = err))
+        return;
+    if (!(await CreateTcpClient(ip, port))
+        .UnWrap(out TcpClient tcpClient, err => errMsg = err))
+        return;
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+}
+finally
+{
+    Console.WriteLine(errMsg ?? "成功");
+    Console.WriteLine("执行结束");
+    Console.ReadLine();
+}
 
 
-
-
-static IResult<string,string> GetIpInfo() {
+static IResult<string, string> GetIpInfo()
+{
     return Success<string, string>.Of("127.0.0.1");
 }
 static IResult<int, string> GetPortInfo()
 {
     return Success<int, string>.Of(8080);
+}
+static async Task<IResult<TcpClient, string>> CreateTcpClient(string ip, int port)
+{
+    try
+    {
+        var tcpClient = new TcpClient();
+        await tcpClient.ConnectAsync(ip, port);
+        return Success<TcpClient, string>.Of(tcpClient);
+    }
+    catch (Exception ex)
+    {
+        return Error<TcpClient, string>.Of(ex.Message);
+    }
 }
 
 
